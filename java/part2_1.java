53721216da8bd985215cb6a2323f59533be6f7b7
leeyongyu <jmk154@naver.com>
2022-06-01 19:04:20 +0900

    2022-06-01 알고리즘 코딩테스트 commit

---
+++ java/part2_1.java
@@ -0,0 +1,27 @@
+package algorithm;
+import java.util.*;
+public class part2_1 {
+
+	public static void main(String[] args) {
+		// 큰 수 출력하기
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		int [] numArr = new int[n];
+		for(int i=0; i<n; i++) {
+			numArr[i] = input.nextInt();
+		}
+		solution(numArr,n);
+	}
+	private static void solution(int [] array, int n) {
+		String result ="";
+		for(int i=0; i<array.length; i++) {
+			if(i==0) {
+				result+= array[i] + " ";
+			}else if(array[i] > array[i-1]){
+				result += array[i] + " ";
+			}
+		}
+		System.out.print(result);
+	}
+}
